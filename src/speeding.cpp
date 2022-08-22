98d8f181ed3ad59c010aa661b40657152b785693
Withanage Perera <78891315+WithanagePerera@users.noreply.github.com>
2022-08-22 14:07:40 -0400

    Chapter 6 project called speeding.cpp

---
+++ src/speeding.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+static const float base_fine = 60.25;
+
+int main()
+{
+
+}
+
+void logInfo()
+{}
+
+float calculateFine(int speed)
+{
+    int speedLimit = 
+    if (speed > speedLimit)
+    {
+        if (speed < speedLimit + 5)
+            return base_fine;
+        else if (speed < speedLimit + 10)
+            return base_fine*1.05;
+        else if (speed < speedLimit + 20)                                                                                                                                                                                                                                                                                                                                                                                                                                                                               )
+            return base_fine*1.1;
+        else
+            reutn base_fine*1.2;
+    }
+    else
+    {
+        return 0;
+    }
+}
