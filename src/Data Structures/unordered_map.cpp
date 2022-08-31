b2600e1ad52e6fd04009d0313284cf1d25312df8
Withanage Perera <78891315+WithanagePerera@users.noreply.github.com>
2022-08-31 12:18:59 -0400

    Unordered maps.

---
+++ src/Data Structures/unordered_map.cpp
@@ -0,0 +1,19 @@
+// Stores elements in key/value pairs
+// Reference key to retrieve values
+// Implemented using Hash Table
+
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+int main()
+{
+    // Initialize unordered map
+    unordered_map<int, string> speed;
+    
+    // Insert values
+    speed.insert({1, "C"});
+    speed.insert({2, "C++"});
+}
