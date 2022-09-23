19a794011f71d7818b912a8e7dbf8f155ae8a4c6
Withanage Perera <78891315+WithanagePerera@users.noreply.github.com>
2022-09-23 14:08:25 -0400

    Started work on tries.

---
+++ src/Data Structures/tries.cpp
@@ -0,0 +1,14 @@
+// Efficient info retrieval data structure
+// Every node consists of multiple branches
+// Each branch represents a possible character of keys
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int ALPHABET_SIZE = 26;
+
+struct TrieNode
+{
+    
+}
