f341bbc05bf0cbc724a33b1f5666f3cdfc62fd25
Withanage Perera <78891315+WithanagePerera@users.noreply.github.com>
2022-09-27 12:27:06 -0400

    Pairs.

---
+++ src/Data Structures/pairs.cpp
@@ -0,0 +1,20 @@
+// Combine together 2 values that may be different data types
+// Way to 2 store 2 values as a single unit
+// Basically a tuple, but can only store 2 values
+
+#include <iostream>
+#include <string>
+#include <array>
+
+using namespace std;
+
+int main()
+{
+    array<int, 10> languages;
+
+    for (int i = 0; i < languages.size(); i++)
+    {
+        languages[i] = 
+    }   
+    pair<string, int> 
+}
