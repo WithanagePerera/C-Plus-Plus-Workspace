3ed3813f59ff092c4cceac0fb28b5734dbbd70e7
Withanage Perera <78891315+WithanagePerera@users.noreply.github.com>
2022-10-01 16:21:23 -0400

    Work on tries and lambdas.

--- src/Data Structures/tries.cpp
+++ src/Data Structures/tries.cpp
@@ -2,7 +2,7 @@
 // Every node consists of multiple branches
 // Each branch represents a possible character of keys
 
-#include <bits/stdc++.h>
+#include <string>
 
 using namespace std;
 
@@ -10,5 +10,29 @@ const int ALPHABET_SIZE = 26;
 
 struct TrieNode
 {
-    
+    struct TrieNode*children[ALPHABET_SIZE];
+
+    bool isEndOfWord;
+};
+
+struct TrieNode *getNode()
+{
+    struct TrieNode *pNode = new TrieNode;
+    pNode->isEndOfWord = false;
+
+    for (int i = 0; i < ALPHABET_SIZE; i++)
+        pNOde->children[i] = NULL;
+
+    return pNode;
+}
+
+void insert (struct TrieNode *root, string key)
+{
+    struct TrieNode *pCrawl = root;
+
+    for (int i = 0; i < key.length(); i++)
+    {
+        int index = key[i] - 'a';
+        if (!pCrawl ->children[index])
+    }
 }
