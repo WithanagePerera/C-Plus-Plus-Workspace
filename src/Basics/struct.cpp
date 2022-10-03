8c227394f5d7bfaebceb389fdc6e35da6ebbc857
Withanage Perera <78891315+WithanagePerera@users.noreply.github.com>
2022-10-03 12:24:21 -0400

    Structs.

---
+++ src/Basics/struct.cpp
@@ -0,0 +1,12 @@
+// User defined data type 
+// Creates a data type that can be used to group items of possibly different types into a single type
+// Can contain data members and member functions
+
+#include <iostream>
+
+using namespace std;
+
+struct Person()
+{
+    int age, weight, height;
+}
