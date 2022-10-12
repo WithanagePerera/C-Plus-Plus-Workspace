2e3f00f4b4756bddcba063c7be97192b5e88ebb5
Withanage Perera <prav.perera@gmail.com>
2022-10-12 18:37:31 -0400

    Started work on heap.

---
+++ src/Data Structures/heap.cpp
@@ -0,0 +1,6 @@
+// Heap is a special tree-based data structure where the tree is a complete binary tree
+// The binary search tree is an ordered data structure, the heap is not
+// THat means in memory, the heap is usually represented as an array of numbers
+// The heap can either be a min-heap or a max-heap
+// The difference is that the root of the tree is the largest number for the max-heap
+// The root is the smallest number for the min-heap
