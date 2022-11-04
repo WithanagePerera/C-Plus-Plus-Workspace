0e4f42c359e79b9c6aaccddd86b51589f836be4a
Withanage Perera <78891315+WithanagePerera@users.noreply.github.com>
2022-11-04 13:36:28 -0400

    Bitmaskin

---
+++ src/Memory/bitmask.cpp
@@ -0,0 +1,4 @@
+// Bitmasking is when an integer is used for its binary representation
+// It's used to represent a subset
+// Say you 8 bits, the n'th bit refers to the n'th element in a subset
+// So if that bit is on or off, that translates to the element in the subset
